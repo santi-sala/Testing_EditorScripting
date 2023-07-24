822950831fd53ad0e5ccb741f1028db3ef1ee236
Santiago Sala <pipo0716@gmail.com>
2023-07-24 03:09:08 +0300

    07 - Adding some style to the custom window

--- TestingEDitorScripting/Assets/Editor/InternalToolkit.cs
+++ TestingEDitorScripting/Assets/Editor/InternalToolkit.cs
@@ -11,6 +11,48 @@ public class InternalToolkit : EditorWindow
 
     private void OnGUI()
     {
-        
+        EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+        EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+        GUILayout.Label("Game Toolkit", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Make Blue"))
+        {
+
+        }
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Make Red"))
+        {
+
+        }
+
+        if (GUILayout.Button("Make Green"))
+        {
+
+        }
+        GUILayout.EndHorizontal();
+        GUILayout.EndVertical();
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+        GUILayout.Label("Spawning", EditorStyles.boldLabel);
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Spawn Sphere"))
+        {
+
+        }
+
+        if (GUILayout.Button("Spawn Cube"))
+        {
+
+        }
+        GUILayout.EndHorizontal();
+
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndVertical();
     }
 }
